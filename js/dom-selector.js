7b53b9bc61ba254c4947aec17622f25489ce2e18
MasumHossain71 <masumhazi311@gmail.com>
2022-12-09 19:32:47 +0600

    some codeare added

--- js/dom-selector.js
+++ js/dom-selector.js
@@ -1,3 +1,17 @@
 // Option-1/ Document.getElementsByTagName
 let var1 = document.getElementsByTagName('tagName');
-for (let var2 of var1)
+for (let var2 of var1){
+   console.log(var2.innerHTML);
+}
+
+// Option-2/ Document.getElementById
+let var3 = document.getElementById('idName');
+for (let var4 of var3){
+   console.log(var4.innerHTML);
+}
+
+// Option-3/ Document.getElementsByClassName
+let var5 = document.getElementsByClassName('className');
+for (let var6 of var5){
+   console.log(var6.innerHTML);
+}
